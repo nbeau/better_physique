6aeed87446a13deb31adb9c0db50be835f455a1e
Nicholas Beaulieu <hockeynick2@gmail.com>
2023-06-30 09:08:30 -0400

    testing

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -32,7 +32,10 @@ function Home() {
                         </div>
                         <div className="row home_spacing">
                             <div className="col-6 d-flex justify-content-end">
-                                <Button text="Training" link="/training"/>
+                                <a href="/training">
+                                    Training
+                                    {/* <Button text="Training" link="/training"/> */}
+                                </a>
                             </div>
                             <div className="col-6 d-flex justify-content-start">
                                 <Button text="Dieting" link="/dieting" />
